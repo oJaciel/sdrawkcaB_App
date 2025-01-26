3e8b14cbd11d1188be839693ba35a04e8cf6bc4a
oJaciel <jacielhbarichellO@gmail.com>
2025-01-25 22:49:59 -0300

    Testando o Provider
    Inicializado o provider na tela de resultado e testados os métodos.
    
    *Apenas para teste, depois irá mudar

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:sdrawkcab/providers/tts_provider.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Instanciando e inicializando o Tts
+    final provider = TtsProvider();
+    provider.initTts();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Resultado'),
+      ),
+      body: FloatingActionButton(
+        onPressed: () =>
+            provider.speak('Teste'),
       ),
     );
   }
