97132e1fce64b95f783f6f2f78ed16fa788736a4
oJaciel <jacielhbarichellO@gmail.com>
2025-01-26 21:34:17 -0300

    Componente Resultado
    Extraído o componente de resultado da tela de resultado.
    
    Próximo passo será unificar tudo em apenas uma tela, facilitando a utilização do app.

---
+++ lib/components/result_component.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sdrawkcab/providers/tts_provider.dart';
+
+class ResultComponent extends StatelessWidget {
+  const ResultComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+//Instanciando e inicializando o Tts
+    final provider = TtsProvider();
+    provider.initTts();
+
+    // Recebendo o texto do formulário
+    final text = ModalRoute.of(context)?.settings.arguments as String?;
+
+    //Função para reverter o texto recebido
+    String reverseText() {
+      //Se o texto for nulo (nunca vai ser), retorna null
+      if (text == null || text.isEmpty) {
+        return 'Valor nulo';
+      }
+      //Senão, reverte o texto
+      List<String> characters = text.split('');
+      // Reverse the order and join the characters
+      String reversedString = characters.reversed.join('');
+      return reversedString;
+    }
+
+    return Column(
+      children: [
+        Text(reverseText()),
+        FloatingActionButton(
+          onPressed: () => provider.speak(reverseText()),
+          child: Text('Falar'),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,44 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:sdrawkcab/providers/tts_provider.dart';
+import 'package:sdrawkcab/components/result_component.dart';
 
 class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //Instanciando e inicializando o Tts
-    final provider = TtsProvider();
-    provider.initTts();
-
-    // Recebendo o texto do formulário
-    final text = ModalRoute.of(context)?.settings.arguments as String?;
-
-    //Função para reverter o texto recebido
-    String reverseText() {
-      //Se o texto for nulo (nunca vai ser), retorna null
-      if (text == null || text.isEmpty) {
-        return 'Valor nulo';
-      }
-      //Senão, reverte o texto
-      List<String> characters = text.split('');
-      // Reverse the order and join the characters
-      String reversedString = characters.reversed.join('');
-      return reversedString;
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Resultado'),
       ),
-      body: Column(
-        children: [
-          Text(reverseText()),
-      FloatingActionButton(
-        onPressed: () => provider.speak(reverseText()),
-        child: Text('Falar'),
-      ),
-
-        ],
-      ) 
-        
+      body: ResultComponent(),
     );
   }
 }
