15365a5cf4725b060afbeed678e2d77565edf083
oJaciel <jacielhbarichellO@gmail.com>
2025-01-26 21:29:47 -0300

    Programação tela de resultado
    Programada a lógia da tela de resultado, com a reversão e fala da palavra enviada.

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -2,22 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:sdrawkcab/providers/tts_provider.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     //Instanciando e inicializando o Tts
     final provider = TtsProvider();
     provider.initTts();
 
+    // Recebendo o texto do formulário
+    final text = ModalRoute.of(context)?.settings.arguments as String?;
+
+    //Função para reverter o texto recebido
+    String reverseText() {
+      //Se o texto for nulo (nunca vai ser), retorna null
+      if (text == null || text.isEmpty) {
+        return 'Valor nulo';
+      }
+      //Senão, reverte o texto
+      List<String> characters = text.split('');
+      // Reverse the order and join the characters
+      String reversedString = characters.reversed.join('');
+      return reversedString;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Resultado'),
       ),
-      body: FloatingActionButton(
-        onPressed: () =>
-            provider.speak('Teste'),
+      body: Column(
+        children: [
+          Text(reverseText()),
+      FloatingActionButton(
+        onPressed: () => provider.speak(reverseText()),
+        child: Text('Falar'),
       ),
+
+        ],
+      ) 
+        
     );
   }
 }
