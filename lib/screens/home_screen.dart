33dc7d8085f3d7dfaa6ea7d5af92bc587a4d7f66
oJaciel <jacielhbarichellO@gmail.com>
2025-02-04 21:48:39 -0300

    Exclusão FormComponent
    Excluído o FormComponent, e adicionados seus elementos para a HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:sdrawkcab/components/form_component.dart';
+import 'package:provider/provider.dart';
+import 'package:sdrawkcab/components/result_component.dart';
+import 'package:sdrawkcab/providers/history_provider.dart';
 
 class HomeScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final formController = TextEditingController();
+
+  _openResultModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return ResultComponent(formController.text);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FormComponent();
+    final provider = Provider.of<HistoryProvider>(context);
+
+    return Form(
+      key: _formKey,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: formController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira algum texto';
+                  }
+                  return null;
+                },
+                textAlign: TextAlign.center,
+                decoration:
+                    const InputDecoration(hintText: "Digite uma palavra"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _openResultModal(context);
+                    provider.addPhraseToHistory(formController.text);
+                  }
+                },
+                child: const Text(
+                  'Enviar',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
