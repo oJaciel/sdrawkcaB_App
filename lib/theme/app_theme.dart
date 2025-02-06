19979de363a59ce0d497bd943044383985ef3c30
oJaciel <jacielhbarichellO@gmail.com>
2025-02-06 19:21:42 -0300

    Atualização do Tema
    Atualizado o tema da aplicação.
    
    Adicionados temas para componentes específicos da aplicação.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,7 +2,45 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final ThemeData appTheme = ThemeData(
-    colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.orange)
-        .copyWith(secondary: Colors.white)
+    scaffoldBackgroundColor: Colors.grey[100],
+    colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepOrange)
+        .copyWith(secondary: Colors.white),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.deepOrange,
+      titleTextStyle: TextStyle(
+        color: Colors.deepOrange,
+        fontSize: 24,
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Colors.deepOrange,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.grey[200],
+      selectedLabelStyle: TextStyle(
+        fontSize: 16,
+      ),
+    ),
+  );
+
+  static final InputDecoration formTextStyle = InputDecoration(
+    hintText: "Digite uma palavra/frase",
+    border: UnderlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(15)),
+    ),
+    filled: true,
+    fillColor: Colors.grey[100],
+    contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+  );
+
+  static final formButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: Colors.deepOrange,
+    foregroundColor: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 15),
+    textStyle: const TextStyle(
+      fontSize: 16,
+    ),
   );
 }
